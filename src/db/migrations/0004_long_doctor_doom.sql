41083c9e369034f3f918628276442e4ab0e3070f
Rodrigo Silverio <rodrigo.s.silverio@outlook.com>
2026-07-20 15:05:48 -0300

    feat: add Node.js server frameworks

---
+++ src/db/migrations/0004_long_doctor_doom.sql
@@ -0,0 +1,3 @@
+ALTER TYPE "public"."framework" ADD VALUE 'express' BEFORE 'django';--> statement-breakpoint
+ALTER TYPE "public"."framework" ADD VALUE 'fastify' BEFORE 'django';--> statement-breakpoint
+ALTER TYPE "public"."framework" ADD VALUE 'nestjs' BEFORE 'django';
